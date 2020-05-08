44fd3bb62eba346e2789f83de1fdc72cc5aa3451
Tony Eneh <aeneh90@gmail.com>
2020-05-08 11:55:33 +0100

    Write database configuration file constants

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+//db connection connection uri
+export const MONGODB_URI = process.env.MONGODB_URI;
+
+//the car owners mongodb collection
+export const DB_CAROWNERS_COLLECTION = process.env.DB_CAROWNERS_COLLECTION;
